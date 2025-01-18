3e2fae94ef2734ee9ecf51af33bd03f6cd07898c
Johnathon Crowder <139363360+JohnathonCrowder@users.noreply.github.com>
2025-01-18 16:22:25 -0600

    Update index.ts
    UPdated cors

--- simmons-family-junk-removal/backend/src/index.ts
+++ simmons-family-junk-removal/backend/src/index.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    'https://codefusionlabs-frontend.onrender.com',
+    'https://simmonsfamilyjunkremoval-frontend.onrender.com/',
     'http://localhost:5173',
     'http://localhost:3000'
   ],
