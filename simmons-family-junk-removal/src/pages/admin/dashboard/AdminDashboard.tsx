98bf46a92c5c50f4ffcdbdf79e25e6759e811dd2
Johnathon Crowder <139363360+JohnathonCrowder@users.noreply.github.com>
2025-01-19 17:32:56 -0600

    Update AdminDashboard.tsx

--- simmons-family-junk-removal/src/pages/admin/dashboard/AdminDashboard.tsx
+++ simmons-family-junk-removal/src/pages/admin/dashboard/AdminDashboard.tsx
@@ -8,9 +8,8 @@ import ControlPanel from "./components/ControlPanel";
 import PostsTable from "./components/PostsTable";
 import PostsGrid from "./components/PostsGrid";
 import DeleteModal from "./components/DeleteModal";
-//import NewsletterSection from "./components/NewsletterSection";
-import ContactSubmissions from "./components/ContactSubmissions";
 import NewsletterSection from "./components/NewsletterSection";
+import ContactSubmissions from "./components/ContactSubmissions";
 
 // New ServerSwitch component
 const ServerSwitch: React.FC<{
